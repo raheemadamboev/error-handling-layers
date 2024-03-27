19c844ed492b468793d17f7988038cc5ed23a1b6
raheemadamboev <vividricemaryandmax@gmail.com>
2024-03-27 19:48:04 +0500

    Removed test directories;

--- app/src/main/java/xyz/teamgravity/errorhandlinglayers/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/errorhandlinglayers/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.errorhandlinglayers.ui.theme.ErrorHandlingLayersTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
